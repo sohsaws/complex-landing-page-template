07fc0a63c9b931f3f66fd56bc9961912f808a0cd
sohsaws <booleansd@proton.me>
2025-12-30 12:41:03 +0700

    Major Updates

--- src/components/InteractiveScene.jsx
+++ src/components/InteractiveScene.jsx
@@ -1,11 +1,25 @@
-import React, { useRef, useMemo, useEffect, useState } from 'react'
+import React, { useRef, useMemo, useEffect, useState, Suspense } from 'react'
 import { Canvas, useFrame, useThree } from '@react-three/fiber'
 import { Environment, Float } from '@react-three/drei'
-import * as THREE from 'three'
+import { Physics, usePlane, useBox } from '@react-three/cannon'
+import Cube from './Cube'
 
 
+export default function InteractiveScene() {
 
+    return (
+        <Canvas>
+            <Physics>
+                <Suspense fallback={null}>
+                    <Cube />
+                </Suspense>
+            </Physics>
+            <ambientLight intensity={1} />
+            <directionalLight position={[1, 1, 1]} intensity={1} />
+        </Canvas>
 
+    )
+}
 
 
 
